53a4137922b02eb124d1a54c586c613212ab2386
adrioro <artistsvillage@me.com>
2017-05-23 10:36:36 -0400

    Addedd plus and minus buttons.

--- app/src/main/java/com/example/android/lesson1/MainActivity.java
+++ app/src/main/java/com/example/android/lesson1/MainActivity.java
@@ -29,10 +29,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        int quantity = 5;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
 
     /**
      * This method displays the given price on the screen.
