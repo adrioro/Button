6ad13348df9b386a8272b90b6d635bfb175f3177
adrioro <artistsvillage@me.com>
2017-07-17 10:05:10 -0400

    Added check boxes Whipped cream and Chocolate

--- app/src/main/java/com/example/android/lesson1/MainActivity.java
+++ app/src/main/java/com/example/android/lesson1/MainActivity.java
@@ -9,10 +9,10 @@ package com.example.android.lesson1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,13 +27,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    /**
+     * Order summary text.
+     */
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "\nName: Adri";
+        priceMessage = priceMessage + "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage = priceMessage + "\nAdd chocolate? " + addChocolate;
+        priceMessage = priceMessage + "\nQuantity:" + quantity;
+        priceMessage = priceMessage + "\nTotal: $" + calculatePrice();
+        priceMessage = priceMessage + "\nThank you!";
+        return priceMessage;
+    }
+
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Price $" + (quantity * 5) + "\nThank you!";
+        /**
+         * Whipped cream check box
+         */
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        /**
+         * Delete the Log.v later
+         */
+        Log.v("MainActivity", "Has whipped cream:" + hasWhippedCream);
+
+
+        /**
+         * Chocolate check box
+         */
+        CheckBox ChocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = ChocolateCheckBox.isChecked();
+        /**
+         * Delete the Log.v later
+         */
+        Log.v("MainActivity", "Has chocolate:" + hasChocolate);
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
+
     }
+
 
     /**
      * This method is called when the plus button is clicked.
@@ -53,19 +104,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
 
     }
 
